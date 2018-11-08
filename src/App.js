5af6d91782044d3013ff6ac5adf1ac677088d856
Sandra Tourjman <sandratourjman@Sandras-MacBook-Pro.local>
2018-11-07 18:48:07 -0600

    commit 1 with component folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
 import './App.css';
+import RoomList from './components/RoomList';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBV8uOfWmqfPGhJtUvF3ZzZuanLIvrpTF8",
+    authDomain: "bloc-chat-4c3dd.firebaseapp.com",
+    databaseURL: "https://bloc-chat-4c3dd.firebaseio.com",
+    projectId: "bloc-chat-4c3dd",
+    storageBucket: "bloc-chat-4c3dd.appspot.com",
+    messagingSenderId: "652757721493"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <RoomList firebase={firebase} />
       </div>
     );
   }
