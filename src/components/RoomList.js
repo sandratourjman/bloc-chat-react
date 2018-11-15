eb8f893809ecec6369b5e72bdbf98b1fb6fbd318
Sandra Tourjman <sandratourjman@Sandras-MacBook-Pro.local>
2018-11-15 17:11:52 -0600

    Checkpoint 2: Add rooms to firebase

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,10 +5,28 @@ class RoomList extends Component {
 		super(props);
 
 		this.state = {
-			rooms: []
+			rooms: [],
+			name: ''
 		};
 
 		this.roomsRef = this.props.firebase.database().ref('rooms');
+	}
+
+	createRoom(newRoomName) {
+		this.roomsRef.push({
+  			name: newRoomName
+		});
+		this.setState({
+			name: ''
+		});
+
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		this.createRoom(e.target.elements.name.value);
+		let form = document.getElementById("room-form");
+		form.reset();
 	}
 
 	componentDidMount() {
@@ -29,6 +47,12 @@ class RoomList extends Component {
 					<li key={room.key}>{room.name}</li>
 					)}
 				</ul>
+
+				<form id="room-form" onSubmit={(e) => this.handleSubmit(e)}>
+ 					<p> Add a room</p>
+  					<input type="text" name="name" />
+  					<input type="submit" value="Submit" />
+				</form> 
 			</div>
 		);
 	}
